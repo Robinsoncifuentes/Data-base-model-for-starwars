34cdc49cc6d0e0a64188bb81cc7fcb707d3cf7f6
Robinsoncifuentes <robinsonmunoz4@gmail.com>
2022-02-22 14:14:19 +0000

    starwars data base

--- src/models.py
+++ src/models.py
@@ -8,23 +8,74 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(250), nullable=False)
+    user_lastname = Column(String(250), nullable=False)
+    email = Column(String(250), unique=True, nullable=False)
+
+class Character(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    homeworld = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    specie = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+
+class Planet(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    location = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+
+class Specie(Base):
+    __tablename__ = 'species'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    location = Column(String(250), nullable=False)
+    homeworld = Column(String(250), nullable=False)
+    lenguage = Column(String(250), nullable=False)
+
+class Starship(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    manufacturer = Column(String(250), nullable=False)
+
+
+
+class FavoritsCharacter(Base):
+    __tablename__ = 'favoritscharacters'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+
+
+class FavoritsPlanet(Base):
+    __tablename__ = 'favoritsplanets'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planets_id = Column(Integer, ForeignKey('planets.id'))
+
+
+class FavoritsStarship(Base):
+    __tablename__ = 'favoritsStarships'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    starships_id = Column(Integer, ForeignKey('starships.id'))
+
+
+class FavoritsSpecie(Base):
+    __tablename__ = 'favoritsSpecies'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    species_id = Column(Integer, ForeignKey('species.id'))
 
     def to_dict(self):
         return {}
